19d0e33fc04e5c48c64e25760a5ee09edb79d797
Rishav Raj <rishavraj1707music@gmail.com>
2023-05-07 12:29:16 +0530

    [feat] Changed the API server

--- Top-Headlines/src/App.jsx
+++ Top-Headlines/src/App.jsx
@@ -4,25 +4,35 @@ import TopHeadlines from "./TopHeadlines";
 import news from "./assets/news.png";
 import "./App.css";
 
-const AUTH_ENDPOINT = "https://newsapi.org/v2/top-headlines?";
-const COUNTRY = "in";
-const PAGESIZE = "100";
-const APIKEY = "976540986a6b48cd9b24fdadfd866626";
+const APIKEY = "4b1d40ec5bmshfe7226d999ac2d5p14a68ajsn59a93ecb7052";
+const APIHOST = "news-api14.p.rapidapi.com";
+const URL = "https://news-api14.p.rapidapi.com/top-headlines";
 
 function App() {
   const [topHeadlines, setTopHeadlines] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const options = {
+      method: "GET",
+      url: `${URL}`,
+      params: {
+        country: "in",
+        language: "en",
+      },
+      headers: {
+        "X-RapidAPI-Key": `${APIKEY}`,
+        "X-RapidAPI-Host": `${APIHOST}`,
+      },
+    };
+
     async function getTopHeadlines() {
       try {
-        const response = await axios.get(
-          `${AUTH_ENDPOINT}country=${COUNTRY}&pageSize=${PAGESIZE}&apiKey=${APIKEY}`
-        );
+        const response = await axios.request(options);
         setTopHeadlines(response.data.articles);
         setLoading(false);
       } catch (error) {
-        console.log("Error:", error);
+        console.error(error);
       }
     }
 
--- Top-Headlines/src/TopHeadlines.jsx
+++ Top-Headlines/src/TopHeadlines.jsx
@@ -2,11 +2,11 @@
 export default function TopHeadlines({ topHeadlines }) {
   return (
     <div className="article-list">
-      {topHeadlines.map(({ urlToImage, title, url }, index) => (
-        <div key={`${title}-${index}`} className="article-item">
-          <img src={urlToImage} />
+      {topHeadlines.map(({ title, url, publisher }, index) => (
+        <div key={index} className="article-item">
           <div className="article-item-body">
             <h4>{title}</h4>
+            <p>by {publisher.name}</p>
             <a href={url} target="_blank" rel="noreferrer">
               Read More
             </a>
